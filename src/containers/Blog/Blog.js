beb0770806634772613cff3732d78e29fb384563
Uladzimir Markau <umarkau@itsupportme.com>
2018-05-22 14:47:34 +0300

    Add asynchronous loading for routes Fix routing issues

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,10 +2,18 @@ import React, { Component } from 'react';
 // import axios from 'axios';
 import Posts from './Posts/Posts';
 import './Blog.css';
-import {Route, NavLink, Switch} from 'react-router-dom';
-import NewPost from './NewPost/NewPost';
+import {Route, NavLink, Switch, Redirect} from 'react-router-dom';
+// import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
+const AsyncNewPost = asyncComponent(() => {
+  return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
+
+  state = {
+    auth: true
+  }
 
     render () {
 
@@ -15,7 +23,7 @@ class Blog extends Component {
                 <nav>
                   <ul>
                     <li><NavLink
-                      to='/posts/'
+                      to='/posts'
                       exact
                       activeClassName='my-active'
                       activeStyle={{
@@ -31,8 +39,10 @@ class Blog extends Component {
                 </nav>
               </header>
               <Switch>
-                <Route path='/new-post' component={NewPost} />
+                {this.state.auth ? <Route path='/new-post' component={AsyncNewPost} /> : null}
                 <Route path='/posts' component={Posts} />
+                {/*<Route render={ () => <h1> Not Found </h1> } />*/}
+                <Redirect from='/' to='/posts' />
               </Switch>
             </div>
         );
